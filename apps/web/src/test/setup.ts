04a35d12c08d3c92562bb0e9dfcd4f47094ade5f
Rapha Bolet <rapha.bolet@gmail.com>
2026-07-08 16:43:19 -0700

    scaffolding and deps for shadcn

--- apps/web/src/test/setup.ts
+++ apps/web/src/test/setup.ts
@@ -1,6 +1,9 @@
 import { cleanup } from "@testing-library/react"
-import { afterEach } from "vitest"
+import { afterEach, expect } from "vitest"
 import "@testing-library/jest-dom/vitest"
+import * as axeMatchers from "vitest-axe/matchers"
+
+expect.extend(axeMatchers)
 
 afterEach(() => {
   cleanup()
